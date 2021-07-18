dd43ed391fdf753bce6ae7a8719402ac780aeb29
Dave Speer <dave.r.speer@gmail.com>
2021-07-18 13:04:59 -0500

    setup: attempt to point the server location to the right place (that doesn't exist yet) and setup for watching csharp files

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -5,41 +5,53 @@
 // tslint:disable
 'use strict';
 
-import { workspace, Disposable, ExtensionContext } from 'vscode';
+import { workspace, ExtensionContext, languages } from 'vscode';
 import { LanguageClient, LanguageClientOptions, SettingMonitor, ServerOptions, TransportKind, InitializeParams } from 'vscode-languageclient';
 import { Trace } from 'vscode-jsonrpc';
+import { join } from 'path';
+import { release } from 'os';
+
+const vsCodeName = 'boilerMoq';
+const friendlyName = 'Boiler Moq';
 
 export function activate(context: ExtensionContext) {
 
     // The server is implemented in node
-    let serverExe = 'dotnet';
+    const serverExe = 'dotnet';
+    
+    const debugServerLocation = join("server", "BoilerMoq", "bin", "Debug", "netcoreapp3.1", "win10-x64", "BoilerMoq.exe");
+    const releaseServerLocation = join("server", "BoilerMoq", "bin", "Release", "netcoreapp3.1", "publish", "BoilerMoq.dll");
+    const debugServer = context.asAbsolutePath(debugServerLocation);
+    const releaseServer = context.asAbsolutePath(releaseServerLocation);
 
     // If the extension is launched in debug mode then the debug server options are used
     // Otherwise the run options are used
-    let serverOptions: ServerOptions = {
-        run: { command: serverExe, args: ['C:\\Users\\mb\\src\\gh\\lsp-example\\Server\\Server\\bin\\Debug\\netcoreapp2.1\\Server.dll'] },
-        debug: { command: serverExe, args: ['C:\\Users\\mb\\src\\gh\\lsp-example\\Server\\Server\\bin\\Debug\\netcoreapp2.1\\Server.dll'] }
-    }
+    const serverOptions: ServerOptions = {
+        run: { command: serverExe, args: [releaseServer] },
+        debug: { command: serverExe, args: [debugServer] }
+    };
 
     // Options to control the language client
-    let clientOptions: LanguageClientOptions = {
+    const clientOptions: LanguageClientOptions = {
         // Register the server for plain text documents
         documentSelector: [
             {
-                pattern: '**/*.csproj',
+                scheme: "file",
+                language: 'csharp'
             }
         ],
         synchronize: {
-            // Synchronize the setting section 'languageServerExample' to the server
-            configurationSection: 'languageServerExample',
-            fileEvents: workspace.createFileSystemWatcher('**/*.csproj')
+            // Synchronize the setting section for our extension to the server
+            configurationSection: vsCodeName,
+            fileEvents: workspace.createFileSystemWatcher('**/*.cs')
         },
-    }
+    };
 
     // Create the language client and start the client.
-    const client = new LanguageClient('languageServerExample', 'Language Server Example', serverOptions, clientOptions);
+    const client = new LanguageClient(vsCodeName, friendlyName, serverOptions, clientOptions);
+
     client.trace = Trace.Verbose;
-    let disposable = client.start();
+    const disposable = client.start();
 
     // Push the disposable to the context's subscriptions so that the
     // client can be deactivated on extension deactivation
