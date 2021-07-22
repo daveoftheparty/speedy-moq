39d40a59edbd2480a15703e51d4ed6ae812755b2
Dave Speer <dave.r.speer@gmail.com>
2021-07-21 20:35:37 -0500

    progress?? extension doesn't seem to crash, but not sure i have logging configured correctly, and, i have no listeners/handlers yet...

--- server/BoilerMoq/Program.cs
+++ server/BoilerMoq/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+using OmniSharp.Extensions.LanguageServer.Server;
 
 namespace BoilerMoq
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			var server = await LanguageServer.From(options =>
+				options
+				.WithInput(Console.OpenStandardInput())
+				.WithOutput(Console.OpenStandardOutput())
+				.WithLoggerFactory(new LoggerFactory())
+				.AddDefaultLoggingProvider()
+				.WithServices(x => x.AddLogging(b => b.SetMinimumLevel(LogLevel.Trace)))
+				.WithServices(ConfigureServices)
+				);
+
+			await server.WaitForExit;
+		}
+
+		static void ConfigureServices(IServiceCollection services)
+		{
+		}
+	}
 }
