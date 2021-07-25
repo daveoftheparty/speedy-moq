afb263c82dd2eaf506d6a6ccf967e3e3dc48b6c0
Dave Speer <dave.r.speer@gmail.com>
2021-07-25 00:22:53 -0500

    pseudo: notes

--- server/Features/MoqGenerator/Generator.cs
+++ server/Features/MoqGenerator/Generator.cs
@@ -19,10 +19,12 @@ namespace Features.MoqGenerator
 				if ! bool IHasFileChanged(uri fileUri, string text) // store a dictionary of file URI and MD5?
 					return orig text / text + IsChanged bool sentinel?
 
+				interfacename = IMockText.GetInterfaceName(text); // roslyn syntax or semantic model here to find the raw interface in the test text
+
 				if bool IMockText.AlreadyMocked(uri fileUri, string interfaceName) // store a dictionary of file URI and list of mocked interfaces?
 
 
-				mockText = string IMockText.IGetMock(uri fileUri, string text) // add the "new" interface[s] to the IAlreadyMocked dict
+				mockText =   IMockText.IGetMock(uri fileUri, string text) // add the "new" interface[s] to the IAlreadyMocked dict
 				
 				insert mockText into original input text and return...
 			*/
