b0eff0b9c4b96ea1d70a32244b7d8bd83a3dbe95
Dave Speer <dave.r.speer@gmail.com>
2021-07-25 16:31:18 -0500

    DRY: refactored file test case reading in prep for MockTests

--- server/UnitTests.Features/MoqGenerator/FullGenerationTests.cs
+++ server/UnitTests.Features/MoqGenerator/FullGenerationTests.cs
@@ -11,15 +11,14 @@ namespace UnitTests.Features.MoqGenerator
 	public class FullGenerationTests
 	{
 		[TestCaseSource(nameof(FileTests))]
-		public async Task Go((string input, string expected, string testId) test)
+		public async Task Go((string testId, string input, string expected) test)
 		{
 			var generator = new Generator();
 
 			var actual = await generator.GetReplacementAsync(Guid.NewGuid().ToString(), test.input);
 			
-			var testMessage = $"TestId: {test.testId}";
-			Assert.IsTrue(actual.IsChanged, testMessage);
-			Assert.AreEqual(test.expected, actual.Text, testMessage);
+			Assert.IsTrue(actual.IsChanged, test.testId);
+			Assert.AreEqual(test.expected, actual.Text, test.testId);
 		}
 
 		public static IEnumerable<(string input, string expected, string testId)> FileTests
@@ -27,34 +26,11 @@ namespace UnitTests.Features.MoqGenerator
 			get
 			{
 				const string path = "TestData/FullGenerationTests/";
+				var data = TestDataReader.GetTests(path);
 
-				var files = Directory
-					.EnumerateFiles(path)
-					.Select(f => 
-					{
-						var fileOnly = f.Replace(path, "");
-						return new
-						{
-							TestNumber = fileOnly.Substring(0,3),
-							FileName = fileOnly
-						};
-					})
-					.GroupBy(t => t.TestNumber)
-					.OrderBy(o => o.Key)
-					.Select(g => 
-					{
-						var files = g.Select(n => n.FileName).ToList();
-						return new
-						{
-							Input = files[1],
-							Expected = files[0],
-							TestId = g.Key
-						};
-					});
-
-				foreach(var file in files)
+				foreach(var test in data)
 				{
-					yield return (File.ReadAllText(path + file.Input), File.ReadAllText(path + file.Expected), file.TestId);
+					yield return (test.testId, test.tests[1], test.tests[0]);
 				}
 			}
 		}
