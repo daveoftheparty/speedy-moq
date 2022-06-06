94dc22245c13559fbb6e8ca010903c493e873037
Dave Speer <dave.r.speer@gmail.com>
2022-06-05 23:41:28 -0500

    red: InterfaceGenericsBuilder -- but gonna let it stand red for a bit while working through the rest of the codebase

--- server/MoqGenerator.UnitTests/InterfaceGenericsBuilderTests.cs
+++ server/MoqGenerator.UnitTests/InterfaceGenericsBuilderTests.cs
@@ -6,11 +6,16 @@ namespace MoqGenerator.UnitTests
 	public class InterfaceGenericsBuilderTests
 	{
 		[TestCase("IList<int>", "IList", "int")]
+		[TestCase("IGenericService<TSource, TResult>", "IGenericService", "TSource", "TResult")]
+		[TestCase("IService<T, K, V>", "IService", "T", "K", "V")]
+		[TestCase("IService\t<    T,\r\tK,V\r\n>", "IService", "T", "K", "V")]
+		[TestCase("IService<TSource, IReadOnlyDictionary<TResult>>", "IService", "TSource", "IReadOnlyDictionary<TResult>")]
 		public void BuildSlowest(string code, string expectedName, params string[] expectedArgs)
 		{
 			var builder = new InterfaceGenericsBuilder();
 			var actual = builder.BuildSlowest(code);
 			
+			Assert.AreEqual(1, actual.Count);
 			Assert.True(actual.ContainsKey(expectedName));
 			CollectionAssert.AreEqual(expectedArgs, actual[expectedName].GenericTypeArguments);
 		}
