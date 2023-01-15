6da1061f6d6710cc0c11da531ddf106503b3c4b2
daveoftheparty <71727192+daveoftheparty@users.noreply.github.com>
2023-01-15 17:36:34 -0600

    Update server/MoqGenerator/Interfaces/Lsp/IInterfaceGenericsBuilder.cs

--- server/MoqGenerator/Interfaces/Lsp/IInterfaceGenericsBuilder.cs
+++ server/MoqGenerator/Interfaces/Lsp/IInterfaceGenericsBuilder.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using Microsoft.CodeAnalysis;
 using MoqGenerator.Model;
 
-#warning elevate interfaces up one level before? done with this branch, no need for everyting to be in Interfaces.Lsp -- or do a separate branch just for that
 namespace MoqGenerator.Interfaces.Lsp
 {
 	public interface IInterfaceGenericsBuilder
