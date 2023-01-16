8419a403b9bf670ad60c4a29f10855804f2f858c
Dave Speer <dave.r.speer@gmail.com>
2023-01-15 23:58:25 -0600

    green: got the weird smashed using c# working

--- server/MoqGenerator/Services/TestFileFilter.cs
+++ server/MoqGenerator/Services/TestFileFilter.cs
@@ -25,7 +25,27 @@ public class TestFileFilter : ITestFileFilter
 		{
 			foreach(var framework in _testFrameworks)
 			{
-				if(line.Trim().StartsWith(framework))
+				/*
+				well, this is awkward, because these are easy enough to test for:
+					using Xunit; // see this trailing comment?
+					using System;using Xunit;
+				
+				and so is this, make sure there isn't an in-line comment preceding:
+					// using System;usingXunit;
+
+				but I really don't feel like coding for a block comment
+				
+					/<imagine a *>
+					...
+					using System;usingXunit;
+					...
+					<imagine a *>/
+
+				and really, is it the end of the world if we THINK it's a test file and it's not?
+				Diagnoser will just output codegen suggestions for non-test files...
+				*/
+
+				if(line.IndexOf(framework) >= 0)
 					return true;
 			}
 		}
